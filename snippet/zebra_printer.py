63c3f2b7505c22a904e4993cab28bddf17e69c04
locchuong <locchuong123@gmail.com>
2022-09-30 16:15:19 +0700

    test printing to printer

---
+++ snippet/zebra_printer.py
@@ -0,0 +1,28 @@
+label = """
+^XA
+
+^FO140,15
+^A0,40,40
+^FD
+Total Weight : 50 KG
+^FS
+
+^FO140,60
+^A0,40,40
+^FD
+shipment_count: 34
+^FS
+
+^FO140,105
+^A0,40,40
+^FD
+HUB: DEL
+^FS
+
+
+^XZ
+"""
+
+from zebra import zebra
+z = zebra('zebra_test')
+z.output(label)
