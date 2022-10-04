478d1bd4882e230e2f65ad4f2768013de1a2fbb6
locchuong <locchuong123@gmail.com>
2022-10-05 05:46:55 +0700

    building printer service

--- printer_service.py
+++ printer_service.py
@@ -1,4 +1,28 @@
 import win32print 
 import win32ui
-from PIL import Image,ImageWin
 import time
+import json
+from PIL import Image,ImageWin
+
+class printer():
+    def __init__(self):
+        self.OFFSET_X_PIXEL = 0 # offset x position
+        self.OFFSET_Y_PIXEL = 0 # offset y position
+        self.SCALE_SIZE = 0.5 # scale factor
+        self.printer_name = win32print.GetDefaultPrinter() # get printer default
+
+# Init
+file_path = "./docs/task_printer.json"
+
+# Loop
+
+#Send request and Load json
+f = open(file_path) # read text
+tasks_printer = json.load(f) # convert text to json
+#print(tasks_printer)
+
+#Generate image
+
+#Print
+
+#Delay
