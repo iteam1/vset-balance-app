9227e8f6d29a15fd588846c5363a4ac83b7e09d4
locchuong <locchuong123@gmail.com>
2022-10-04 16:19:56 +0700

    done balance service

--- balance_service.py
+++ balance_service.py
@@ -1,6 +1,8 @@
 import serial
 import sqlite3
 import time
+import requests
+import json
 
 conn = sqlite3.connect("./root/site.db")
 c = conn.cursor()
@@ -29,11 +31,26 @@ class mybalance():
         self.conn.commit()
         
 the_balance = mybalance(conn = conn)
+
+value = 0.0
+base_url = 'https://vset.vvs.vn/parse/classes/GoldWeight/IXvPHGKTen'
+data = {"value":value}
+headers = {
+    'X-Parse-Application-Id':'SCWASRTWK1Y9AVMP1KFC',
+}
+
+def update_api(value):
+    with requests.Session() as s:
+        data = {"value":float(value)}
+        res = s.put(base_url,headers=headers,data=json.dumps(data))
+        print(res.json())
     
 if __name__ == "__main__":
     while True:
         comming_data = my_serial.read(14) # read():read on byte, read(14): read up to 14 bytes, readline() readline
         my_string = comming_data.decode()
-        print(my_string.strip())
+        value = my_string.strip().split("T")[0]
+        print(f"value: {value}")
         the_balance.update_value(query_string,my_string)
+        update_api(value)
         time.sleep(0.1)
