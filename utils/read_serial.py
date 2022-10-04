6a89d3007eb31bf76adc53f6223761261e538011
locchuong <locchuong123@gmail.com>
2022-10-04 07:21:23 +0700

    edit read_serial util

--- utils/read_serial.py
+++ utils/read_serial.py
@@ -4,9 +4,13 @@ import serial
 my_com = input("Enter your serial-com (Default COM3): ") 
 if my_com == "": my_com = "COM3"
 
-my_serial = serial.Serial(my_com)
-print(f"Serial COM= {my_serial.name}") # check com port
-
+try:
+    my_serial = serial.Serial(my_com)
+    print(f"Serial COM = {my_serial.name} Connected!") # check com port
+except Exception as e:
+    print(e)
+    exit() # exit if error
+    
 if __name__ == "__main__":
     while True:
         comming_data = my_serial.read(14) # read():read on byte, read(14): read up to 14 bytes, readline() readline
